2697181949ebe655ff25974fec5369afef98f59b
«Ekaterina» <katuxa-muxa@mail.ru>
2022-11-23 20:49:03 +0200

    задача 3

---
+++ hw3/Program.cs
@@ -0,0 +1,13 @@
+﻿bool main (int num1)
+{
+    if (num1 % 7 ==0 || num1 % 6 ==0)
+    
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+Console.WriteLine($"--{main(1)}");
