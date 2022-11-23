d2b3b2a448f6bcd64049ada58eb3438fc55a22bd
«Ekaterina» <katuxa-muxa@mail.ru>
2022-11-23 20:48:11 +0200

    задача 1

---
+++ hw1/Program.cs
@@ -0,0 +1,12 @@
+﻿int FindNum(int num)
+{
+    int sot = num % 100;
+    int ost = sot / 10;
+    
+    return ost;
+}
+int random = new Random().Next(100,1000);
+int newNumber = FindNum(random);
+Console.WriteLine($"new version of {random} is {newNumber}");
+
+
