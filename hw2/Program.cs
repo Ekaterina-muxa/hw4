6e7162f43b173f3bd0ca563a957d47f22703ca7a
«Ekaterina» <katuxa-muxa@mail.ru>
2022-11-23 20:48:41 +0200

    задача 2

---
+++ hw2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+void main (int num)
+{
+    
+    int sot = num % 100;
+    if (sot > 10)
+    {
+       Console.WriteLine("третьей цифры нет");
+      // int ed = num % 10;
+      // Console.WriteLine(ed);
+    }
+    else
+     {
+       int ed = num % 10;
+       Console.WriteLine(ed);
+       // Console.WriteLine("третьей цифры нет");
+    }
+}
+main (385);
